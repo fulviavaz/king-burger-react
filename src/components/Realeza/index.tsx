b46a4694b4a8c0722217542245bb47f577d521f9
fulviavaz <fulviavaz@hotmail.com>
2023-09-25 15:21:41 -0300

    create slider

--- src/components/Realeza/index.tsx
+++ src/components/Realeza/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Realeza.module.scss";
 import RealezaItem from "./RealezaItem";
+import Slider from "./Slider";
+import { SwiperSlide } from "swiper/react";
 
 export default function Realeza() {
   const [data, setData] = useState([
@@ -18,6 +20,15 @@ export default function Realeza() {
       .then((data) => setData(data));
   }, []);
 
+  const settings = {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    navigation: true,
+    pagination: {
+      clickable: true,
+    },
+  };
+
   return (
     <section id="comentarios" className={styles.realeza}>
       <h2 className={styles.realeza__title}>Nossa realeza</h2>
@@ -25,17 +36,36 @@ export default function Realeza() {
         A satisfação de nossos clientes em primeiro lugar!
       </p>
       <div className={styles.realeza__container}>
-        {data.length > 0 &&
-          data.map((item) => (
-            <RealezaItem
-              key={item.name}
-              name={item.name}
-              image={item.image}
-              age={item.age}
-              testimonial={item.testimonial}
-            />
-          ))}
+        <Slider settings={settings}>
+          <SwiperSlide>
+            {data.length > 0 &&
+              data.map((item) => (
+                <RealezaItem
+                  key={item.name}
+                  name={item.name}
+                  image={item.image}
+                  age={item.age}
+                  testimonial={item.testimonial}
+                />
+              ))}
+          </SwiperSlide>
+        </Slider>
       </div>
+      {/* 
+      <Slider settings={settings}>
+        <SwiperSlide>
+          {data.length > 0 &&
+            data.map((item) => (
+              <RealezaItem
+                key={item.name}
+                name={item.name}
+                image={item.image}
+                age={item.age}
+                testimonial={item.testimonial}
+              />
+            ))}
+        </SwiperSlide>
+      </Slider> */}
     </section>
   );
 }
