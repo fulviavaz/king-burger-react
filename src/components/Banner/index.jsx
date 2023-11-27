8b6f17c3daf8f0b1f6fb6e0c5395a17a7dc8adf1
fulviavaz <fulviavaz@hotmail.com>
2023-11-27 12:05:13 -0300

    fix reponsiveness at banner on mobile

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import style from "./Banner.module.scss";
-import kingburger from "../../assets/kingburger.png";
 import Menu from "components/Menu";
 
 export default function Banner() {
   return (
     <section id="home" className={style.banner}>
       <div className={style.banner__content}>
-        <h1 className={style.banner__title}>Uma nova experiência!</h1>
-        <img src={kingburger} alt="King Burger" />
+        <p className={style.banner__title}>Uma nova experiência!</p>
+        <h1 className={style.banner__principal}>
+          KING <span>BURGER</span>
+        </h1>
         <p className={style.banner__text}>
           Para quem tem um <span>Apetite de um REI!</span>
         </p>
